212270348670c1a479c134a36d35fa03fe3aeb0a
Rodrigo Leite <rods.leite27@gmail.com>
2020-12-27 10:14:55 -0300

    Updates

---
+++ precache-manifest.99a857c8117b2ad1ac821aae5bc60178.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbbd04a0d5248e8ee7dfe8330e07d86c",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "0c484a458f4c2c7a0d55",
+    "url": "/frontend-online-store/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "f4ddc1578f2e3070d799",
+    "url": "/frontend-online-store/static/css/main.b7d648af.chunk.css"
+  },
+  {
+    "revision": "0c484a458f4c2c7a0d55",
+    "url": "/frontend-online-store/static/js/2.0151aa08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-online-store/static/js/2.0151aa08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ddc1578f2e3070d799",
+    "url": "/frontend-online-store/static/js/main.067c336e.chunk.js"
+  },
+  {
+    "revision": "a2d408d9505863c5fc2c",
+    "url": "/frontend-online-store/static/js/runtime-main.2928fc3e.js"
+  }
+]);
