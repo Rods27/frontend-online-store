a43c467ac224c52c713fd247ff594702fa5910a8
Rodrigo Leite <rods.leite27@gmail.com>
2021-02-06 10:56:49 -0300

    shoppingCart, products, productDetails, header, categorySlider upgraded to SCSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store/index'
 import { BrowserRouter } from 'react-router-dom';
+import './styles/main.scss'
 
 ReactDOM.render(
   <Provider store={ store }>
